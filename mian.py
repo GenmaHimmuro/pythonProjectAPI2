77a2e23522d5f42ad93a56c451e85d06a6e7526d
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-01-13 17:42:30 +0500

    API

--- mian.py
+++ mian.py
@@ -4,31 +4,39 @@ from dotenv import load_dotenv
 from urllib.parse import urlparse
 
 
-def is_short_link(link):
-    parsed_url = urlparse(link)
-    if parsed_url.netloc == "vk.cc" and len(parsed_url.path) > 1:
-        return True
-    return False
+def is_short_link(link,token):
+    payload = {
+        "access_token": token,
+        "v": '5.199',
+        "key": (urlparse(link)).path[1:],
+        "interval": "forever",
+        "intervals_count": 1
+    }
+    url = 'https://api.vk.com/method/utils.getLinkStats'
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    if 'error' in response.json():
+        return False
+    return True
 
 
-def get_vk_link(token, link):
+def get_link_vk_format(token, link):
     payload = {
         'url' : link,
         'access_token' : token,
         'v':'5.199',
     }
-    url='https://api.vk.com/method/utils.getShortLink'
+    url = 'https://api.vk.com/method/utils.getShortLink'
     response = requests.get(url, params=payload)
     response.raise_for_status()
     return response.json()
 
 
-def get_link_stats(token,short_link):
-    short_vk_link = (urlparse(short_link)).path[1:]
+def get_quantity_clicks(token,short_link):
     payload = {
         "access_token": token,
         "v": '5.199',
-        "key": short_vk_link,
+        "key": (urlparse(short_link)).path[1:],
         "interval": "forever",
         "intervals_count": 1
     }
@@ -40,20 +48,20 @@ def get_link_stats(token,short_link):
 
 def main():
     load_dotenv()
-    token = os.environ['TOKEN']
+    token = os.environ['VK_TOKEN']
     link = input('Введите адрес: ')
 
 
     try:
 
 
-        if is_short_link(link):
-            short_link = link
-            print('Просмотров',get_link_stats(token, short_link)['response']['stats'][0]['views'])
+        if is_short_link(link,token):
+                short_link = link
+                print('Просмотров',get_quantity_clicks(token, short_link)['response']['stats'][0]['views'])
         else:
-            short_link = get_vk_link(token, link)['response']['short_url']
-            print('Сокращённая ссылка:', get_vk_link(token, link)['response']['short_url'])
-            print('Просмотров',get_link_stats(token, short_link)['response']['stats'][0]['views'])
+                short_link = get_link_vk_format(token, link)['response']['short_url']
+                print(f'Сокращённая ссылка: {short_link}',
+                      '\nПросмотров:',get_quantity_clicks(token, short_link)['response']['stats'][0]['views'])
 
 
     except requests.exceptions.RequestException as e:
